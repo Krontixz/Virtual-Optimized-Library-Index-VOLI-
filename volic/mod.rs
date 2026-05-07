3987c1467572ce5adf7c474979563e26f2da476c
Krontixz <zaki.coding0119@gmail.com>
2026-05-06 18:07:21 -0700

    Create mod.rs

---
+++ volic/mod.rs
@@ -0,0 +1,7 @@
+pub mod types;
+pub mod core;
+pub mod bytecode_handlers;
+
+pub use crate::volic::types::VolicType;
+pub use crate::volic::core::VolicClass;
+pub use crate::volic::core::VolicHeader;
