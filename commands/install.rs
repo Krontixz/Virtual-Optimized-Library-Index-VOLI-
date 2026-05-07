0cdf96bffb77cb95d4e5f0f8dd6abd612d0172c5
Krontixz <zaki.coding0119@gmail.com>
2026-05-07 09:08:06 -0700

    Create install.rs

---
+++ commands/install.rs
@@ -0,0 +1,42 @@
+use std::fs;
+use std::env;
+use std::path::PathBuf;
+use std::io::Result;
+
+pub struct VoliInstaller {
+    pub home_dir: PathBuf,
+}
+
+impl VoliInstaller {
+    pub fn new() -> Self {
+        let mut path = env::current_dir().unwrap();
+        path.push(".voli");
+        Self { home_dir: path }
+    }
+
+    pub fn setup_environment(&self) -> Result<()> {
+        if !self.home_dir.exists() {
+            fs::create_dir_all(&self.home_dir)?;
+        }
+
+        let bin_path = self.home_dir.join("bin");
+        let lib_path = self.home_dir.join("lib");
+        let cache_path = self.home_dir.join("cache");
+
+        fs::create_dir_all(bin_path)?;
+        fs::create_dir_all(lib_path)?;
+        fs::create_dir_all(cache_path)?;
+
+        Ok(())
+    }
+
+    pub fn register_extension(&self) -> Result<()> {
+        #[cfg(target_os = "windows")]
+        {
+            use std::process::Command;
+            Command::new("assoc").arg(".volic=VolicFile").output()?;
+            Command::new("ftype").arg("VolicFile=voli.exe %1").output()?;
+        }
+        Ok(())
+    }
+}
